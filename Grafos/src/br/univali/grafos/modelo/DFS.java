d6c422ff4acd15ee43bbc31aaeddce3da7540730
Ailtonjr <Ailton Jr@Ailton>
2015-08-20 17:28:38 -0300

    Ultima alteração: esta funcionando a leitura do grafo corretamente, falta a busca da vertice e dizer se é conexo ou não

--- Grafos/src/br/univali/grafos/modelo/DFS.java
+++ Grafos/src/br/univali/grafos/modelo/DFS.java
@@ -8,6 +8,7 @@ import java.util.Stack;
 public class DFS {
 
     Stack<Vertice> pilha = new Stack<>();
+    Stack<Vertice> pilhaRetorno = new Stack<>();
 
     public void busca(MeuGrafo grafo, String rotulo){
         int cont = 0;
@@ -23,8 +24,13 @@ public class DFS {
         if(cont > 1){
             System.out.println("Não é conexo!");
         }
+        System.err.println("tamanho pilha retorno "+ pilhaRetorno.size());
+        for (Vertice pilhaRetorno : pilhaRetorno) {
+            System.out.println("Retorno ---------------------------------- " + pilhaRetorno.rotulo);
+        }
     }
     public void percorrer(String rotulo) {
+        
         System.out.println("Empilhou " + pilha.lastElement().rotulo);
         while (!pilha.empty()) {
             if (!pilha.lastElement().arcos.isEmpty()) {
@@ -32,6 +38,11 @@ public class DFS {
                     if (!arco.verticeB.visitado) {
                         System.out.println("Empilhou " + arco.verticeB.rotulo);
                         pilha.push(arco.verticeB);
+                        if(pilha.lastElement().rotulo.equals(rotulo) ){
+                            for (Vertice pilha1 : pilha) {
+                                System.err.println(pilha1.rotulo);
+                            }
+                        }
                         arco.verticeB.visitado = true;
                         break;
                     } else {
