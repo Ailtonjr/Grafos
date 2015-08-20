75f1a1654984fc4d329e6a8cc73128c9c2ea56dc
Ailtonjr <Ailton Jr@Ailton>
2015-08-20 15:35:30 -0300

    Ultima alteração

--- Grafos/src/br/univali/grafos/modelo/DFS.java
+++ Grafos/src/br/univali/grafos/modelo/DFS.java
@@ -1,4 +1,3 @@
-
 package br.univali.grafos.modelo;
 
 import br.univali.grafos.principal.Arco;
@@ -6,37 +5,35 @@ import br.univali.grafos.principal.MeuGrafo;
 import br.univali.grafos.principal.Vertice;
 import java.util.Stack;
 
-
 public class DFS {
-    
+
     Stack<Vertice> pilha = new Stack<>();
-    
+
     public void percorrer(MeuGrafo grafo, String rotulo) {
         pilha.push(grafo.vertices.get(0));
-        System.out.println("Empilhou " + pilha.lastElement());
-        while (!pilha.empty()) {    
-            int cont = pilha.firstElement().arcos.size();
-            //System.out.println("primeiro elemento "+ pilha.firstElement().rotulo);
-            
-            if(!pilha.lastElement().arcos.isEmpty()){
-                System.out.println("ultimo"+ pilha.lastElement().rotulo);
-                for (Arco arco : pilha.firstElement().arcos) {
-                    if(arco.verticeB.visitado != true && cont != 0){
+        pilha.lastElement().visitado = true;
+        System.out.println("Empilhou " + pilha.lastElement().rotulo);
+        while (!pilha.empty()) {
+            int cont = pilha.lastElement().arcos.size();
+            if (!pilha.lastElement().arcos.isEmpty()) {
+                for (Arco arco : pilha.lastElement().arcos) {
+                    if (!arco.verticeB.visitado && cont != 0) {
                         System.out.println("cont " + cont);
                         cont--;
                         System.out.println("Empilhou " + arco.verticeB.rotulo);
                         pilha.push(arco.verticeB);
                         arco.verticeB.visitado = true;
                         break;
-                    }else{
+                    } else {
                         System.out.println("Desempilhou " + pilha.lastElement().rotulo);
                         pilha.pop();
                     }
                 }
+            } else {
+                System.out.println("Desempilhou2 " + pilha.lastElement().rotulo);
+                pilha.pop();
             }
-            
         }
         System.out.println("fim ciclo");
-
     }
 }
