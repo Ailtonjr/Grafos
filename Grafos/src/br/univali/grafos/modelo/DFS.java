441e1bcaba94d579c24784179272272b255dda4a
Ailtonjr <Ailton Jr@Ailton>
2015-08-20 14:49:30 -0300

    inicio

--- Grafos/src/br/univali/grafos/modelo/DFS.java
+++ Grafos/src/br/univali/grafos/modelo/DFS.java
@@ -13,28 +13,30 @@ public class DFS {
     
     public void percorrer(MeuGrafo grafo, String rotulo) {
         pilha.push(grafo.vertices.get(0));
-        System.out.println("Empilhou" + grafo.vertices.get(0).rotulo );
+        System.out.println("Empilhou " + pilha.lastElement());
         while (!pilha.empty()) {    
             int cont = pilha.firstElement().arcos.size();
-
-            if(!pilha.firstElement().arcos.isEmpty()){
+            //System.out.println("primeiro elemento "+ pilha.firstElement().rotulo);
+            
+            if(!pilha.lastElement().arcos.isEmpty()){
+                System.out.println("ultimo"+ pilha.lastElement().rotulo);
                 for (Arco arco : pilha.firstElement().arcos) {
                     if(arco.verticeB.visitado != true && cont != 0){
                         System.out.println("cont " + cont);
                         cont--;
                         System.out.println("Empilhou " + arco.verticeB.rotulo);
-                        arco.verticeB.visitado = true;
                         pilha.push(arco.verticeB);
+                        arco.verticeB.visitado = true;
                         break;
                     }else{
-                        System.out.println("Desempilhou " + pilha.firstElement().rotulo);
+                        System.out.println("Desempilhou " + pilha.lastElement().rotulo);
                         pilha.pop();
                     }
                 }
-            }else{
-                System.out.println("fim ciclo");
-                break;
             }
+            
         }
+        System.out.println("fim ciclo");
+
     }
 }
