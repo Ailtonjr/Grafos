e5e6d274be92e7a8a6026f4f6be1267128023b06
Ailtonjr <ailtoncardoso0@gmail.com>
2015-09-26 20:07:13 -0300

    Construido o algoritmo de A* 	Falta resolver: 		- Zerar a lista fechada quando não tiver pra onde ir 		- fazer algoritmo de não repetição de entrada em becos 		- Criar a interface grafica 		- Exibir na interface grafica

--- Grafos/src/br/univali/grafos/modelo/Elemento.java
+++ Grafos/src/br/univali/grafos/modelo/Elemento.java
@@ -2,10 +2,17 @@
 package br.univali.grafos.modelo;
 
 public class Elemento {
-    private String tipo;
-    private int f;
-    private int g;
-    private int h;
+    private String tipo = "-";
+    private int f = 0;
+    private int g = 0;
+    private int h = 0;
+    private int i;
+    private int j;
+
+    public Elemento(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
 
     public String getTipo() {
         return tipo;
@@ -37,5 +44,21 @@ public class Elemento {
 
     public void setH(int h) {
         this.h = h;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
     }
 }
