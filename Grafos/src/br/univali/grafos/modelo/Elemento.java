27c2dc9b7e3c26e280304951ca50b2dae61c161f
Ailtonjr <ailtoncardoso0@gmail.com>
2015-09-26 14:30:35 -0300

    Adicionado estrutura de elemento

---
+++ Grafos/src/br/univali/grafos/modelo/Elemento.java
@@ -0,0 +1,41 @@
+
+package br.univali.grafos.modelo;
+
+public class Elemento {
+    private String tipo;
+    private int f;
+    private int g;
+    private int h;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public void setG(int g) {
+        this.g = g;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+}
