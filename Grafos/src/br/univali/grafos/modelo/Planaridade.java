cbf338b0ae38945b2a51747befd695bdbcd1ee81
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2015-10-02 17:34:00 -0300

    Recursividade monstra, adicionada.

--- Grafos/src/br/univali/grafos/modelo/Planaridade.java
+++ Grafos/src/br/univali/grafos/modelo/Planaridade.java
@@ -1,11 +1,16 @@
 
 package br.univali.grafos.modelo;
 
+import br.univali.grafos.principal.Arco;
 import br.univali.grafos.principal.MeuGrafo;
+import br.univali.grafos.principal.Vertice;
 
 
 public class Planaridade {
 
+    private int cont = 1;
+    private int idCiclo;
+    
     public Planaridade(MeuGrafo grafo) {
         int qtdVertices = grafo.vertices.size();
         int qtdArestas = grafo.todosArcos.size();
@@ -17,7 +22,7 @@ public class Planaridade {
         
         if (qtdVertices >= 3) {
             if (qtdArestas <= (3*qtdVertices) - 6) { // E <= 3*v - 6
-                if (!cicloDeTres()) {   // Se nao houver ciclos de comprimento 3
+                if (!isTemCiclo(grafo)) {   // Se nao houver ciclos de comprimento 3
                     if (qtdArestas <= 2*qtdVertices - 4) {
                         System.out.println("O grafo E PLANAR!");
                     } else {
@@ -35,13 +40,28 @@ public class Planaridade {
         }
     }
     
+    public boolean isTemCiclo(MeuGrafo grafo) {
+        for (Vertice vertice : grafo.vertices) {
+            if(cicloDeTres(vertice))    return true;
+            cont = 1;
+        }
+        return false;
+    }
     
-    public boolean cicloDeTres() {
+    public boolean cicloDeTres(Vertice vertice) {
         // TO DO: Verificar ciclo de comprimento tres
+        if (cont == 1)  idCiclo = vertice.id;
         
-        
-        
-        return true;
+        for (Arco arco : vertice.arcos) {
+            if (cont != 3) {
+                cont ++;
+                cicloDeTres(arco.destino);
+            } else if (arco.destino.id == idCiclo) {    // se contador == 3 e fecha um ciclo com o primeiro
+                return true;
+            }
+        }
+        cont--;
+        return false;
     }
     
     
