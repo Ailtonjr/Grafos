3dd105e2bc8210c0031668265efd33b1cd607e8d
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2015-10-09 16:52:20 -0300

    Ideia de um algoritmo que não entra em buracos que ja entrou, adicionada. Faltando refinar.

---
+++ Grafos/src/br/univali/grafos/modelo/Buraco.java
@@ -0,0 +1,25 @@
+
+package br.univali.grafos.modelo;
+
+
+public class Buraco {
+    private int x;
+    private int y;
+
+    public Buraco(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+    
+    
+}
