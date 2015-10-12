b85d5bca7f0df330997f16d35fa23b8a0d6c9546
Ailtonjr <ailtoncardoso0@gmail.com>
2015-10-12 19:59:39 -0300

    Pequenas modificações: 	- contornado o problema gerado pelo programa GraphMax, que um grafo não direcionado era direcionado para um lado apenas.

--- Grafos/src/br/univali/grafos/principal/Grafo.java
+++ Grafos/src/br/univali/grafos/principal/Grafo.java
@@ -3,33 +3,41 @@ package br.univali.grafos.principal;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Grafo {
-    
+
     public List<Vertice> vertices;
     public List<Arco> todosArcos;
     public boolean dirigido;
-    
-    public Grafo(){
+
+    public Grafo() {
         this.vertices = new ArrayList<>();
         this.todosArcos = new ArrayList<>();
     }
 
     void adicionarVertice(String rotulo, int relId) {
-        vertices.add(new Vertice(rotulo,relId));
+        vertices.add(new Vertice(rotulo, relId));
     }
 
     void adicionarArco(int idVertice1, int idVertice2, double peso) {
-        todosArcos.add(new Arco(vertices.get(idVertice1), vertices.get(idVertice2), peso));
-        vertices.get(idVertice1).arcos.add(new Arco(vertices.get(idVertice1), vertices.get(idVertice2), peso));
+        if (this.dirigido) {
+            todosArcos.add(new Arco(vertices.get(idVertice1), vertices.get(idVertice2), peso));
+            vertices.get(idVertice1).arcos.add(new Arco(vertices.get(idVertice1), vertices.get(idVertice2), peso));
+        } else {
+            todosArcos.add(new Arco(vertices.get(idVertice1), vertices.get(idVertice2), peso));
+            vertices.get(idVertice1).arcos.add(new Arco(vertices.get(idVertice1), vertices.get(idVertice2), peso));
+            
+            
+            todosArcos.add(new Arco(vertices.get(idVertice2), vertices.get(idVertice1), peso));
+            vertices.get(idVertice2).arcos.add(new Arco(vertices.get(idVertice2), vertices.get(idVertice1), peso));
+        }
+
     }
-    
-    void lerVertices(){
+
+    void lerVertices() {
         for (Vertice vertice : vertices) {
             System.out.println("Rotulo: " + vertice.rotulo);
             System.out.println("ID: " + vertice.id);
         }
     }
-    
-    
+
 }
