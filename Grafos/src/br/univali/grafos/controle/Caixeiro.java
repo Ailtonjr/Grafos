83303da9ef842171dd7b819734719bce54919096
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2015-11-07 09:52:51 -0200

    Encontra menor terminado. Adicionado estrutura (List) para acompanhar o circuito do viajante.

--- Grafos/src/br/univali/grafos/controle/Caixeiro.java
+++ Grafos/src/br/univali/grafos/controle/Caixeiro.java
@@ -1,42 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.univali.grafos.controle;
 
 import br.univali.grafos.modelo.Arco;
 import br.univali.grafos.modelo.Grafo;
 import br.univali.grafos.modelo.Vertice;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author Ailton Cardoso Jr
- */
+
 public class Caixeiro {
     private double menor = Double.MAX_VALUE;
     private Grafo grafo;
+    private List<Vertice> circuito = new ArrayList();
 
     public Caixeiro(Grafo grafo) {
         this.grafo = grafo;
         encontraMenor();
+        testaSubciclo();
     }
     
     public void encontraMenor(){
+        Vertice verticeInicial = null;
+        Vertice verticeDestino = null;
+        Vertice aux = null;
+        
         for (Vertice vertice : grafo.vertices) {
             for (Arco arco : vertice.arcos) {
                 double x = arco.peso;
                 for (Arco arcoDestino : arco.destino.arcos) {
                     if(arcoDestino.destino.rotulo.equalsIgnoreCase(vertice.rotulo)){
                         x += arcoDestino.peso;
+                        aux = arcoDestino.origem;
                         break;
                     }
                 }
                 if(x < menor){
                     menor = x;
+                    verticeInicial = vertice;
+                    verticeDestino = aux;
                 }
             }
         }
-        System.out.println("Menor: " + menor);
+        circuito.add(verticeInicial);
+        circuito.add(verticeDestino);
+        System.out.println("Menor: " + menor + "\t" + verticeInicial.rotulo + verticeDestino.rotulo);
+    }
+    
+    public void testaSubciclo() {
+        
     }
 }
