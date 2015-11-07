1693bb52cc74b35d75319ca97d3833f2d66e723d
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2015-11-07 10:40:26 -0200

    Ideia para testaSubciclo adicionada. Verificar dinamificações.

--- Grafos/src/br/univali/grafos/controle/Caixeiro.java
+++ Grafos/src/br/univali/grafos/controle/Caixeiro.java
@@ -16,7 +16,15 @@ public class Caixeiro {
     public Caixeiro(Grafo grafo) {
         this.grafo = grafo;
         encontraMenor();
-        testaSubciclo();
+        
+        // Trecho de codigo a seguir deve ser dinamico
+        Vertice subciclo = testaSubciclo(circuito.get(0), 20, circuito.get(1));    // distancia deve ser dinamica;
+        
+        if (subciclo != null) { // Se e diferente de null, e porque achou um subciclo melhor
+            circuito.add(circuito.indexOf(circuito.get(1)), subciclo);
+        }
+        
+        exibeCircuito();
     }
     
     public void encontraMenor(){
@@ -43,10 +51,31 @@ public class Caixeiro {
         }
         circuito.add(verticeInicial);
         circuito.add(verticeDestino);
+        circuito.get(0).visitado = true;
+        circuito.get(1).visitado = true;
         System.out.println("Menor: " + menor + "\t" + verticeInicial.rotulo + verticeDestino.rotulo);
     }
     
-    public void testaSubciclo() {
+    public Vertice testaSubciclo(Vertice a, double distancia, Vertice b) {
+        Vertice subcicloMenor = null;
         
+        for (Arco  arco: a.arcos) {
+            for (Arco arcoDestino : arco.destino.arcos) {
+                if (arcoDestino.destino.rotulo.equals(b.rotulo)) {
+                    if (arco.peso + arcoDestino.peso - distancia <= distancia) {// Menor ou igual?
+                        subcicloMenor = arco.destino;                           // Captura a referencia do vertice subciclico
+                    }
+                    break;
+                }
+            }
+        }
+        
+        return subcicloMenor;
+    }
+    
+    public void exibeCircuito() {
+        for (Vertice vertice : circuito) {
+            System.out.println(vertice.rotulo + "\t");
+        }
     }
 }
